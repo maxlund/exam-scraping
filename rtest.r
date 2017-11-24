1e639fac21c23832084eac6403750a2514566e99
Max Lund <maxlun@gmail.com>
2017-11-24 12:03:04 +0100

    plots

--- rtest.r
+++ rtest.r
@@ -1,23 +1,27 @@
-data = read.csv("~/python/exam-scraping/exam_results.csv", header = TRUE, 
+data = read.csv("~/exam-scraping/exam_results.csv", header = TRUE, 
                 sep = ",", dec = ".", fill = TRUE)
 
-plot(data$institution, data$U)
-
-exam = data[which.max(data$U),]
-print(exam)
-
+#plot(data$institution, data$U)
 data.ordered = data[order(data$U),]
 
-data.worst = data.ordered[(nrow(data)-100):nrow(data),]
-print(data.worst)
+results.u = tapply(c(data$U), data$course_code, FUN=sum)
+results.3 = tapply(c(data$X3), data$course_code, FUN=sum)
+results.4 = tapply(c(data$X4), data$course_code, FUN=sum)
+results.5 = tapply(c(data$X5), data$course_code, FUN=sum)
 
-plot(data.worst$U, data.worst$X5)
-print(unique(data.worst$course_code)) # courses with top 101 most num of fails
+print(names(results.5))
 
-print(length(which(data.worst$level == "G1"))) # 100 courses are G1
-print(length(which(data.worst$level == "G2"))) # 1 is G2
-print(length(which(data.worst$level == "A")))  # 0 are A
+tmp = data.frame(
+  U = results.u,
+  X3 = results.3,
+  X4 = results.4,
+  X5 = results.5
+)
 
+num_fail = apply(tmp, 1, function(x) {return (x["U"] / sum(x))})
+sorted = sort(num_fail)
 
-courses = unique(data$course_code)
+top_ten = sorted[(length(sorted)-11):length(sorted)-1]
+print(
+plot(top_ten)
 
