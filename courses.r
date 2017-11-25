de423cefa5cce0f2894a0d7442ab981ad8852f7c
Max Lund <maxlun@gmail.com>
2017-11-25 17:17:09 +0100

    r scripts for courses/institution stats

---
+++ courses.r
@@ -0,0 +1,135 @@
+data = read.csv("~/python/exam-scraping/exam_results.csv", header = TRUE, 
+                sep = ",", dec = ".", fill = TRUE)
+
+get_pie=function(name)
+{
+  vals = c(courses[name, ]$U, courses[name, ]$X3, courses[name, ]$X4, courses[name, ]$X5)
+  
+  pie = plot_ly(courses[name, ], labels = c("U", "3", "4", "5"), values = vals, type = 'pie') %>%
+    layout(title = paste("Grade distribution in", name, sep=" "),
+           showlegend = TRUE,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  return (pie)
+}
+
+# sum all exam results within a specific course
+courses = data.frame(
+  U = tapply(c(data$U), data$course_code, FUN=sum),
+  X3 = tapply(c(data$X3), data$course_code, FUN=sum),
+  X4 = tapply(c(data$X4), data$course_code, FUN=sum),
+  X5 = tapply(c(data$X5), data$course_code, FUN=sum)
+)
+
+# get distribution of each grade within courses
+courses.fractions = data.frame(
+  U = apply(courses, 1, function(x) return (x["U"] / sum(x))),
+  X3 = apply(courses, 1, function(x) return (x["X3"] / sum(x))),
+  X4 = apply(courses, 1, function(x) return (x["X4"] / sum(x))),
+  X5 = apply(courses, 1, function(x) return (x["X5"] / sum(x)))
+)
+
+# plot of grade 5 vs grade U
+plot(courses.fractions$U, courses.fractions$X5, xlab="Percentage of grade U", ylab="Percentage of grade 5")
+
+# get the grade U ratio of each course
+courses.fail = apply(courses, 1, function(x) return (x["U"] / sum(x)))
+# get the grade 5 ratio of each course
+courses.success = apply(courses, 1, function(x) return (x["X5"] / sum(x)))
+
+# ten courses with highest ratio of U's
+courses.fail = sort(courses.fail)
+courses.top_fail = courses.fail[(length(courses.fail)-10):length(courses.fail)-1]
+cat("\nTop ten courses with highest ratio of U's:\n")
+print(courses.top_fail)
+
+# ten courses highest ratio of 5's
+courses.success = sort(courses.success)
+courses.top_success = courses.success[(length(courses.success)-11):length(courses.success)-1]
+cat("\nTop ten courses highest ratio of 5's:\n")
+print(courses.top_success)
+
+# courses with no fails
+courses.no_fail = courses.fail[courses.fail == 0]
+cat("\nCourses with 0% fail rate:\n")
+print(courses.no_fail)
+cat("Total:", length(courses.no_fail), "\n")
+
+# -- fitting a polynomial curve to the data
+
+#courses.fractions = courses.fractions[-which(courses.fractions$U == 1), ] # one broken observation/bad data
+#courses.fractions = courses.fractions[-which(courses.fractions$X5 > 0.6), ] # testing without 60%+ ratio of grade 5
+
+set.seed(12345)
+n = nrow(courses.fractions)
+id = sample(1:n, floor(n*0.5))
+train = courses.fractions[id, ]
+test = courses.fractions[-id, ]
+
+mse = numeric(6)
+for (i in 1:16)
+{
+  model = lm(formula = X5 ~ poly(U, i), data=train)
+  predictions = predict(model, test)
+  mse[i] = mean((test$X5 - predictions)^2)
+}
+
+# best polynomial fit at i = 2
+degree = which(mse==min(mse))
+cat("best polynomial at i =",degree)
+model = lm(formula = X5 ~ poly(U, degree), data=train)
+#summary(model)
+preds = predict(model, test)
+plot(test$U, test$X5, col="deepskyblue4", xlab="Percentage of grade U", ylab="Percentage of grade 5")
+points(test$U, preds, col="red")
+legend("bottomright",c("Observ.","Predicted"), 
+       col=c("deepskyblue4","red"), lwd=3)
+
+# plots for some of the courses with highest ratio of U's
+TATA68 = get_pie("TATA68") # TATA68:Matematisk grundkurs 6.0 hp
+TATA68
+TFYA35 = get_pie("TFYA35") # TFYA35:Molekylfysik 6.0 hp
+TFYA35
+TATA43 = get_pie("TATA43") # TATA43:Flervariabelanalys 8.0 hp
+TATA43
+
+# ~94% grade 5 in this course:
+NBID61 = get_pie("NBID61") # NBID61:Primate Ethology 9.0 hp
+NBID61
+
+# subset IDA courses
+ida_ = data[which(data$institution == "IDA"), ]
+ida = data.frame(
+  U = tapply(c(ida_$U), ida_$course_code, FUN=sum),
+  X3 = tapply(c(ida_$X3), ida_$course_code, FUN=sum),
+  X4 = tapply(c(ida_$X4), ida_$course_code, FUN=sum),
+  X5 = tapply(c(ida_$X5), ida_$course_code, FUN=sum)
+)
+
+# get the grade 5 ratio of each IDA course
+ida.fail = apply(ida, 1, function(x) return (x["U"] / sum(x)))
+# get the grade 5 ratio of each IDA course
+ida.success = apply(ida, 1, function(x) return (x["X5"] / sum(x)))
+
+# ten courses with highest ratio of U's
+ida.fail = sort(ida.fail)
+ida.top_fail = ida.fail[(length(ida.fail)-10):length(ida.fail)-1]
+cat("\nTop ten IDA courses with highest ratio of U's:\n")
+print(ida.top_fail)
+
+# ten courses highest ratio of 5's
+ida.success = sort(ida.success)
+ida.top_success = ida.success[(length(ida.success)-11):length(ida.success)-1]
+cat("\nTop ten IDA courses highest ratio of 5's:\n")
+print(ida.top_success)
+
+TDDC75 = get_pie("TDDC75") # TDDC75:Diskreta strukturer 8.0 hp
+TDDC75 = get_pie("TDDD74") # TDDD74:Databaser för bioinformatik 6.0 hp
+TDDD08 = get_pie("TDDD08") # TDDD08:Logikprogrammering 6.0 hp
+TDDD65 = get_pie("TDDD65") # TDDD65:Introduction to the Theory of Computation 6.0 hp
+TDDD65
+TDDD38 = get_pie("TDDD38") # TDDD38:Avancerad programmering i C++ 6.0 hp
+TDDD38
+TDDD07 = get_pie("TDDD07") # TDDD07:Realtidssystem 6.0 hp
+TDDD07
+
---
+++ institutions.r
@@ -0,0 +1,84 @@
+library(plotly)
+
+data = read.csv("~/python/exam-scraping/exam_results.csv", header = TRUE, 
+                sep = ",", dec = ".", fill = TRUE)
+
+get_pie=function(name)
+{
+  vals = c(inst[name, ]$U, inst[name, ]$X3, inst[name, ]$X4, inst[name, ]$X5)
+  
+  pie = plot_ly(inst[name, ], labels = c("U", "3", "4", "5"), values = vals, type = 'pie') %>%
+    layout(title = paste("Grade distribution in", name, sep=" "),
+           showlegend = TRUE,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  return (pie)
+}
+
+inst.u = tapply(c(data$U), data$institution, FUN=sum)
+inst.3 = tapply(c(data$X3), data$institution, FUN=sum)
+inst.4 = tapply(c(data$X4), data$institution, FUN=sum)
+inst.5 = tapply(c(data$X5), data$institution, FUN=sum)
+
+inst = data.frame(
+  U = institution.u,
+  X3 = institution.3,
+  X4 = institution.4,
+  X5 = institution.5
+)
+
+inst.fractions = data.frame(
+  U = apply(inst, 1, function(x) return (x["U"] / sum(x))),
+  X3 = apply(inst, 1, function(x) return (x["X3"] / sum(x))),
+  X4 = apply(inst, 1, function(x) return (x["X4"] / sum(x))),
+  X5 = apply(inst, 1, function(x) return (x["X5"] / sum(x)))
+)
+
+print(rownames(inst))
+
+pie = plot_ly(inst, labels = rownames(inst), values = inst$U, type = 'pie') %>%
+      layout(title = "Distribution of all U's (fail) handed out by all institutions", showlegend = TRUE,
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+pie
+
+pie = plot_ly(inst, labels = rownames(inst), values = inst$X5, type = 'pie') %>%
+      layout(title = "Distribution of all 5's (highest grade) handed out by all institutions", showlegend = TRUE,
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+pie
+
+cat("\nDistribution of grades within institutions:\n")
+print(inst.fractions)
+
+most_fail = inst.fractions[which.max(inst.fractions$U), ]
+cat("\nInstitution with highest ratio of U's:\n")
+print(most_fail)
+
+least_fail = inst.fractions[which.min(inst.fractions$U), ]
+cat("\nInstitution with lowest ratio of U's:\n")
+print(least_fail)
+
+most_fives = inst.fractions[which.max(inst.fractions$X5), ]
+cat("\nInstitution with highest ratio of 5's:\n")
+print(most_fives)
+
+least_fives = inst.fractions[which.min(inst.fractions$X5), ]
+cat("\nInstitution with lowest ratio of 5's:\n")
+print(least_fives)
+
+IDA = get_pie("IDA")
+MAI = get_pie("MAI")
+IKE = get_pie("IKE")
+IKK = get_pie("IKK")
+IEI = get_pie("IEI")
+IDA
+MAI
+IKE
+IKK
+IEI
+
+cat("\nIKK has had a total of one person failing an exam since 2007. This happend on 2017-10-20 in the course TRTE17: Visuell kultur och designhistoria.
+    \nAll exam results within IKK:\n")
+print(data[which(data$institution=="IKK"), ])
+
