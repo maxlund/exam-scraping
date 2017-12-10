4001151f2c6432900e616edb8308203b35c0eca8
Max Lund <maxlun@gmail.com>
2017-12-10 23:12:20 +0100

    update dataset with course names

--- get_scores.py
+++ get_scores.py
@@ -43,6 +43,24 @@ def get_exam_results(course_code):
 
     return all_dates
 
+# this was an afterthought.. nice to have a course's name too
+def get_course_name(course_code):
+    res = requests.get("http://www4.student.liu.se/tentaresult/en/?kurskod=" + course_code + "&search=Search")
+    try:
+        res.raise_for_status()
+    except Exception as e:
+        print("{} raised an exception: {}".format(course_code, e))
+
+    soup = bs.BeautifulSoup(res.text, "lxml")
+
+    try:
+        tables = soup.find_all("table")[4:5][0].text.split(":")
+        course_name = tables[1].split(" ")[:-2]
+        course_name = " ".join(course_name).strip()
+        return course_name
+    except:
+        print("********** {} ********* EXCEPTION THROWN HERE".format(course_code))
+        return ""
 
 all_codes = pickle.load(open("course_codes.p", "rb"))
 
@@ -57,12 +75,13 @@ for institution, levels in all_codes.items():
             for course_code in courses:
                 all_results[institution][level][course_code] = get_exam_results(course_code)
 
+pickle.dump(all_results, open("all_results.p", "wb"))
 print("Done scraping exam results! Going to write to 'exam_results.csv'")
 
 # set up csv file with headers
 f = open("exam_results.csv", "wt")
 writer = csv.writer(f)
-writer.writerow(("id", "institution", "course_code", "level", "date", "U", "3", "4", "5"))
+writer.writerow(("id", "institution", "course_code", "course_name", "level", "date", "U", "3", "4", "5"))
 
 # go through all results and write to our csv file
 id = 1
@@ -70,6 +89,8 @@ result = dict()
 for inst, levels in all_results.items():
     for level, codes in levels.items():
         for code, dates in codes.items():
+            course_name = get_course_name(code)
+            print(course_name)
             for date, grades in dates.items(): # a specific exam processed here
                 result = {"U": 0, "3": 0, "4": 0, "5": 0}
                 for grade, num_of_people in grades.items():
@@ -77,7 +98,7 @@ for inst, levels in all_results.items():
                         result["U"] = num_of_people
                     else:
                         result[grade] = num_of_people
-                writer.writerow((id, inst, code, level, date, result["U"], result["3"], result["4"], result["5"]))
+                writer.writerow((id, inst, code, course_name, level, date, result["U"], result["3"], result["4"], result["5"]))
                 id += 1
 f.close()
 
